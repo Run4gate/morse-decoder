a6bd697a92904ca6df5b9594de204c93b2e8246a
Run4gate <westernisator@gmail.com>
2021-08-23 22:00:11 +0300

    init: start task morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let a = expr.match(/.{1,2}/g).join(' ').match(/.{1,15}/g).join('** ** ** ** ** ').match(/.{1,3}/g)
+    let i = 0
+    let result = ''
+    while (i <= expr.length) {
+    if (a[i] === "10 " || a[i] === '10') {
+            result = result + '.'
+            i++
+    } else if (a[i] === "11 " || a[i] === '11') {
+            result = result + '-'
+            i++
+    } else if (a[i] === "00 ") {
+        result = result + ''
+        i++
+    } else if (a[i] === "** ") {
+        result = result + ' '
+        i += 5
+    }else {
+        break
+    }
+    }
+
+    return result
+    .split('   ')
+    .map(
+      a => a
+        .split(' ')
+        .map(
+          b => MORSE_TABLE[b]
+        ).join('')
+    ).join(' ');
 }
 
 module.exports = {
